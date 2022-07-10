395c1b67458e448477a634888e1db27ae861d2a4
HIROAKI-KAJI <108774332+HIROAKI-KAJI@users.noreply.github.com>
2022-07-10 12:07:51 +0900

    Update K_Widget_sample.py

--- K_Widget_sample.py
+++ K_Widget_sample.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import cv2
 
@@ -12,17 +11,23 @@ def function2(text):
     print(text)
 def functino3(num,num2):
     print(num*num2)
+
+def function4(img):
+    cv2.imwrite("sample.jpg",img)
+    
 def main():
     nullcanvas = np.zeros((1000,1000,3),np.int8)
                           
     rectangle = K_Widget.Rectangle(nullcanvas,[100,100],color=(255,33,40))
     text = K_Widget.Text(nullcanvas,"Text",color = (0,0,255))
-    button =K_Widget.Button(nullcanvas,text="Button",textcolor = (255,0,255),thickness=1,fill =True)
-   
-    rectangle.place(100,0)
-    text.place(100,100)
-    button.place(100,200)
-
+    button =K_Widget.Button(nullcanvas,text="Button",textcolor = (255,0,255),thickness=1)
+    button2 = K_Widget.Button(nullcanvas,text="SAVE IMG",textcolor = (0,0,255),thickness = 1)
+    
+    rectangle.place(100,150)
+    text.place(100,250)
+    button.place(100,350)
+    button2.place(300,150)
+    
     t = 0
     deathtime = 100000000000
 
@@ -31,12 +36,12 @@ def main():
         cv2.imshow("sample frame",nullcanvas)
         #buttonobject activate
         button.active("sample frame",function,function2,functino3,function2 = "Text2",functino3 = (10,4)) 
-        
+        button2.active("sample frame",function4,function4 = nullcanvas)
+        cv2.setMouseCallback("sample frame", K_Widget.mousecallevent,(button,button2))
         key = cv2.waitKey(10) & 0xFF
         if key == ord('s'):
+            
             cv2.destroyAllWindows()
             break        
 if __name__ == '__main__':
     main()
-                      
-                      
